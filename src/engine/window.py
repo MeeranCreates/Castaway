2ed2d20113da49b5a9a4fa967266212798fdb4b1
Meeran <meeranmohi12@gmail.com>
2026-08-14 19:20:37 +0530

    0.0.0

--- src/engine/window.py
+++ src/engine/window.py
@@ -1,38 +1,12 @@
 import glfw
-from OpenGL.GL import glClear, GL_COLOR_BUFFER_BIT
-import sys
 
-def main():
-    # 1. Initialize GLFW
-    if not glfw.init():
-        print("Failed to initialize GLFW", file=sys.stderr)
-        return
+glfw.init()
 
+window = glfw.create_window(1200,720,"Castaway",None,None)
 
-    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+glfw.make_context_current(window)
 
+while not glfw.window_should_close(window):
+    glfw.poll_events()
 
-    window = glfw.create_window(1200, 800, "Castaway", None, None)
-    if not window:
-        print("Failed to create GLFW window", file=sys.stderr)
-        glfw.terminate()
-        return
-
-
-    glfw.make_context_current(window)
-
-    while not glfw.window_should_close(window):
-
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        glfw.swap_buffers(window)
-
-        glfw.poll_events()
-
-    # 6. Clean up and exit
-    glfw.terminate()
-
-if __name__ == "__main__":
-    main()
+glfw.terminate()
